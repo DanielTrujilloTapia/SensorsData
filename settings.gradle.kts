a33efc2c2e186e0a90ed4c91fa194a16e8df3f43
JaviGaming8 <Javier7737302566@gmail.com>
2025-07-09 00:43:18 -0600

    Recuperar cambios del stash temporal

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,12 +12,15 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
+
+
+
 
 rootProject.name = "SensorsData"
 include(":mobile")
